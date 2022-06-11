a7e7e79184cb17550f329509bdc59899703b70e1
Kazbek2303 <Kazbektutinbay@gmail.com>
2022-06-12 00:40:56 +0600

    Added menu

---
+++ Assets/OptionMenu.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+
+public class OptionMenu : MonoBehaviour
+{
+    public AudioMixer audioMixer;
+
+    public void SetVolume(float Volume)
+    {
+        audioMixer.SetFloat("Volume", Mathf.Log10(Volume) * 20);
+    }
+
+    public void SetQuality(int qualityIndex)
+    {
+        QualitySettings.SetQualityLevel(qualityIndex);
+    }
+
+    public void Sound()
+    {
+        AudioListener.pause = !AudioListener.pause;
+    }
+
+
+}
